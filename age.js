5a5d2aeb6bbfe5ccba75ed175df5abbd30d280d6
AndreiDubinin <andrei_dubinin2021@mail.ru>
2022-05-07 16:33:26 +0300

    add age and circle

---
+++ age.js
@@ -0,0 +1,15 @@
+
+let age = 99;
+
+if (age >= 18 && age <60){
+    console.log("Full content")
+}
+else if (age < 18){
+    console.log("Cartoons")
+}
+else if (age >= 60){
+    console.log("Culture")
+}
+else{
+    console.log("Good luck!")
+}
